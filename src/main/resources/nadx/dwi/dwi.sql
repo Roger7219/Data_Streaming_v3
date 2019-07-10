4daae17a9cf549e9aff21cbe7c9d2e3a9b6b12cb
Alex <lilei_happyboy@163.com>
2019-07-10 12:31:08 +0800

    优化

--- src/main/resources/nadx/dwi/dwi.sql
+++ src/main/resources/nadx/dwi/dwi.sql
@@ -813,3 +813,26 @@ ALTER TABLE nadx_overall_traffic_dwi ADD COLUMNS (media_type STRING);
 ALTER TABLE nadx_overall_traffic_dwi_v9 ADD COLUMNS (media_type STRING);
 ALTER TABLE nadx_traffic_dwi_40000 ADD COLUMNS (media_type STRING);
 ALTER TABLE nadx_overall_performance_matched_dwi_v9 ADD COLUMNS (media_type STRING);
+
+
+
+ALTER TABLE nadx_overall_performance_matched_dwi_v6 ADD COLUMNS (raterType STRING);
+ALTER TABLE nadx_overall_performance_matched_dwi_v6 ADD COLUMNS (raterId STRING);
+ALTER TABLE nadx_overall_performance_matched_dwi_v6 ADD COLUMNS (adomain STRING);
+ALTER TABLE nadx_overall_performance_matched_dwi_v6 ADD COLUMNS (rater_type STRING);
+ALTER TABLE nadx_overall_performance_matched_dwi_v6 ADD COLUMNS (rater_id STRING);
+ALTER TABLE nadx_overall_performance_matched_dwi_v6 ADD COLUMNS (media_type STRING);
+
+
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (raterType STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (raterId STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (adomain STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (rater_type STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (rater_id STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (media_type STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (site_domain STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (publisher_id STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (crid STRING);
+ALTER TABLE nadx_overall_dwr_v6 ADD COLUMNS (bidfloor STRING);
+
+
