37436015fa81cbc4692ba2f95612ac2e43af7668
kairenlo <kairenlo@mobikok.com>
2019-07-11 15:08:48 +0800

    fix-bug

--- src/main/scala/com/mobikok/ssp/data/streaming/util/YarnAPPManagerUtil.java
+++ src/main/scala/com/mobikok/ssp/data/streaming/util/YarnAPPManagerUtil.java
@@ -290,7 +290,7 @@ public class YarnAPPManagerUtil {
                     && !excludeAppIds.contains(app.getApplicationId().toString())) {
 
                     filteredApps.add(app);
-                    LOG.warn("Killing filteredApp: " + app.getApplicationId() +", forceKill: " + forceKill);
+                    LOG.warn("Waiting kill app complete, app: " + app.getApplicationId() +", forceKill: " + forceKill);
                 }
             }
 
