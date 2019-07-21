18dad6c67e2123dcaa844fd806bdd9f81f86c5ed
Alex <lilei_happyboy@163.com>
2019-07-21 11:08:07 +0800

    mysql表同步到hive

--- src/main/resources/nadx/sync/sync.sql
+++ src/main/resources/nadx/sync/sync.sql
@@ -1,5 +1,6 @@
 
 -- Hive
+drop table tb_demand_account;
  CREATE TABLE tb_demand_account(
    id                            int,
    tb_user_id                    int,
@@ -14,17 +15,17 @@
    target_source_token_audio     string,
    protocol                      int,
    currency                      string,
-   balance                       int,
+   balance                       double,
    rtb_version                   string,
    markup_method                 int,
    ad_format                     int,
    secure                        int,
    gzip                          int,
    price_model                   string,
-   floor                         int,
+   floor                         double,
    auction_type                  int,
-   second_price_subtract_percent int,
-   second_price_delta            int,
+   second_price_subtract_percent string,
+   second_price_delta            double,
    notify_price_add_percent      int,
    country                       string,
    country_direction             int,
@@ -45,38 +46,63 @@
    sizes                         string,
    sizes_direction               int,
    device_type                   string,
-   media_type                    string
+   media_type                    string,
+   owner_domain_probability      double,
+   owner_bundle_probability      double,
+   owner_publisher_probability   double,
+   floor_percent                 int,
+   demand_bid_rate               double,
+   demand_bid_rate_ratio         double,
+   demand_imp_rate               double,
+   demand_imp_rate_ratio         double,
+   last_updated_time             string
 )
 STORED AS ORC;
 
 
 -- Hive
- CREATE TABLE tb_supply_account(
- id	                int,
- tb_user_id	        int,
- bd_id	            int,
- am_id	            int,
- token	            string,
- name	            string,
- status	            int,
- system_name	    string,
- traffic_type	    int,
- protocol	        int,
- profit_percent	    int,
- price_model	    string,
- currency	        string,
- floor	            int,
- decision_source    int,
- secure	            int,
- company	        string,
- domain	            string,
- country	        string,
- address	        string,
- phone	            string,
- skype	            string,
- wechat	            string,
- max_qps	        int,
- daily_request_cap  int
- )
- STORED AS ORC;
+drop table tb_supply_account;
+CREATE TABLE tb_supply_account (
+  id                    int      ,
+  tb_user_id            int      ,
+  bd_id                 int      ,
+  am_id                 int      ,
+  token                 string   ,
+  name                  string   ,
+  status                int      ,
+  system_name           string   ,
+  traffic_type          int      ,
+  protocol              int      ,
+  profit_percent        double      ,
+  price_model           string   ,
+  currency              string   ,
+  floor                 double   ,
+  decision_source       int      ,
+  secure                int      ,
+  company               string   ,
+  domain                string   ,
+  country               string   ,
+  address               string   ,
+  phone                 string   ,
+  skype                 string   ,
+  wechat                string   ,
+  max_qps               int      ,
+  daily_request_cap     int      ,
+  adomains              string   ,
+  cids                  string   ,
+  crids                 string   ,
+  floor_percent         int   ,
+  last_updated_time     string
+  )
+  STORED AS ORC;
 --where b_date <'2017-09-10';
+
+
+select count(1) from tb_supply_account;
+select count(1) from tb_demand_account;
+
+select * from tb_supply_account limit 1 ;
+select * from tb_demand_account limit 1 ;
+
+
+select max(last_updated_time) from tb_supply_account;
---
+++ src/main/scala/com/mobikok/ssp/data/streaming/handler/dm/offline/SyncMysql2HiveHandlerV3.scala
@@ -0,0 +1,115 @@
+package com.mobikok.ssp.data.streaming.handler.dm.offline
+
+import java.sql.ResultSet
+
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.mobikok.message.client.MessageClient
+import com.mobikok.ssp.data.streaming.client._
+import com.mobikok.ssp.data.streaming.config.RDBConfig
+import com.mobikok.ssp.data.streaming.util.MySqlJDBCClientV2.Callback
+import com.mobikok.ssp.data.streaming.util._
+import com.typesafe.config.Config
+import org.apache.spark.sql.catalyst.analysis.NoSuchTableException
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.{DataFrame, SaveMode}
+
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by admin on 2017/9/4.
+  */
+class SyncMysql2HiveHandlerV3 extends Handler {
+
+  var tableDetails: Map[String, (String, String)] = null //[mysql(hive,uuid)]
+
+  var rdbUrl: String = null
+  var rdbUser: String = null
+  var rdbPassword: String = null
+  var rdbProp: java.util.Properties = null
+
+  val syncIncrMysql2HiveCer = "SyncIncrMysql2HiveCer"
+  val syncUpdateMysql2HiveCer = "SyncUpdateMysql2HiveCer"
+  var updateWhereTopic = Array("config_update")
+
+  var mySqlJDBCClient: MySqlJDBCClientV2 = null
+  @volatile var lock = new Object()
+
+  override def init(moduleName: String, bigQueryClient: BigQueryClient, greenplumClient: GreenplumClient, rDBConfig: RDBConfig, kafkaClient: KafkaClient, messageClient: MessageClient, kylinClientV2: KylinClientV2, hbaseClient: HBaseClient, hiveContext: HiveContext, handlerConfig: Config): Unit = {
+    super.init(moduleName, bigQueryClient, greenplumClient, rDBConfig, kafkaClient, messageClient, kylinClientV2, hbaseClient, hiveContext, handlerConfig)
+
+    tableDetails = handlerConfig.getConfigList("tables").map { x =>
+      x.getString("mysql") -> (x.getString("hive"), if(x.hasPath("uuid")) x.getString("uuid") else null)
+    }.toMap
+
+    rdbUrl = handlerConfig.getString(s"rdb.url")
+    rdbUser = handlerConfig.getString(s"rdb.user")
+    rdbPassword = handlerConfig.getString(s"rdb.password")
+
+    rdbProp = new java.util.Properties {
+      {
+        setProperty("user", rdbUser)
+        setProperty("password", rdbPassword)
+        setProperty("driver", "com.mysql.jdbc.Driver") //must set!
+      }
+    }
+
+    mySqlJDBCClient = new MySqlJDBCClientV2(
+      moduleName, rdbUrl, rdbUser, rdbPassword
+    )
+  }
+
+  override def handle(): Unit = {
+    // 上个批次还在处理时，等待当前批次
+    lock.synchronized{
+      LOG.warn(s"SyncMysql2HiveHandler start")
+
+      tableDetails.entrySet().foreach { x =>
+        var mysqlT = x.getKey;
+        var hiveT = x.getValue._1;
+        var syncResultTmpT = s"${hiveT}_sync_result_tmp"
+
+        val last_updated_time =
+          sql(s"select max(last_updated_time) as last_updated_time from $hiveT")
+            .first()
+            .getAs[String]("last_updated_time")
+        LOG.warn("Incr table ","hiveTable", hiveT, "last_updated_time", last_updated_time)
+        val updateCountDF =
+          hiveContext
+            .read
+            .jdbc(rdbUrl, s"(select count(1) as count from $mysqlT where last_updated_time > '$last_updated_time') as sync_update_table", rdbProp)
+            .first()
+            .getAs[Integer]("count")
+
+        // 全量刷新
+        if (updateCountDF > 0) {
+          sql(s"drop table if exists $syncResultTmpT")
+          sql(s"create table $syncResultTmpT like $hiveT")
+
+          LOG.warn(s"Full table overwrite start", "mysqlTable", mysqlT, "hiveTable", hiveT)
+          var unc = hiveContext.read.table(hiveT)
+
+          hiveContext
+            .read
+            .jdbc(rdbUrl, s"$mysqlT as sync_full_table", rdbProp) //需全表查
+            .selectExpr(unc.schema.fieldNames.map(x => s"`$x`"): _*)
+            .coalesce(1)
+            .write
+            .format("orc")
+            .mode(SaveMode.Overwrite)
+            .insertInto(syncResultTmpT)
+          sql(s"drop table if exists ${hiveT}_backup")         // 删除上次备份
+          sql(s"alter table $hiveT rename to ${hiveT}_backup") // 备份
+          sql(s"alter table $syncResultTmpT rename to ${hiveT}")
+          LOG.warn(s"Full table overwrite done", "mysqlTable", mysqlT, "hiveTable", hiveT)
+        }
+      }
+
+      LOG.warn(s"SyncMysql2HiveHandler done")
+    }
+
+  }
+
+}
