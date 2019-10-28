ef180f0762e4e5e39080c07e2fe5e0bcb1918dc9
kairenlo <kairenlo@mobikok.com>
2019-10-28 12:28:06 +0800

    sdk动态加载修改

---
+++ src/main/resources/sdk/sync/sync.sh
@@ -0,0 +1,35 @@
+spark-submit \
+--name sync_sdk \
+--class com.mobikok.ssp.data.streaming.MixApp \
+--master yarn-cluster \
+--driver-memory 1g \
+--executor-memory 1g \
+--executor-cores 1 \
+--num-executors 1 \
+--queue default \
+--files /usr/hdp/current/spark2-client/conf/hive-site.xml,/apps/data-streaming/sdk/sync/sync.conf \
+--jars \
+file:///apps/data-streaming/libs/metrics-core-2.2.0.jar,\
+file:///apps/data-streaming/libs/spark-streaming-kafka-0-10_2.11-2.2.0.jar,\
+file:///apps/data-streaming/libs/kafka_2.11-0.10.1.0.jar,\
+file:///apps/data-streaming/libs/kafka-clients-0.10.1.0.jar,\
+file:///apps/data-streaming/libs/tephra-api-0.7.0.jar,\
+file:///apps/data-streaming/libs/twill-discovery-api-0.6.0-incubating.jar,\
+file:///apps/data-streaming/libs/twill-zookeeper-0.6.0-incubating.jar,\
+file:///apps/data-streaming/libs/tephra-core-0.7.0.jar,\
+file:///apps/data-streaming/libs/phoenix-core-4.7.0-HBase-1.1.jar,\
+file:///apps/data-streaming/libs/config-1.3.1.jar,\
+file:///apps/data-streaming/libs/hbase-server-1.1.10.jar,\
+file:///apps/data-streaming/libs/mysql-connector-java-5.1.42.jar,\
+file:///apps/data-streaming/libs/hbase-protocol-1.1.3.jar,\
+file:///apps/data-streaming/libs/hbase-common-1.1.3.jar,\
+file:///apps/data-streaming/libs/hbase-client-1.1.10.jar,\
+hdfs:/libs/spark-streaming-kafka-0-10_2.11-2.2.0.jar,\
+hdfs:/libs/kafka_2.11-0.10.1.0.jar,\
+hdfs:/libs/kafka-clients-0.10.1.0.jar,\
+hdfs:/libs/config-1.3.1.jar \
+--driver-class-path /apps/data-streaming/libs/mysql-connector-java-5.1.42.jar \
+--verbose \
+--conf spark.ui.port=5777 \
+/apps/data-streaming/sdk/sync/data-streaming.jar \
+sync.conf kill = true buration = 60
