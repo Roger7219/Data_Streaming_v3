1cb03595e6995f56d7fefae8d18b4a840c20cc4e
kairenlo <kairenlo@mobikok.com>
2020-08-15 00:43:54 +0800

    v3.2

---
+++ src/main/scala/com/mobikok/ssp/data/streaming/util/HeartbeatsTimer.java
@@ -0,0 +1,24 @@
+package com.mobikok.ssp.data.streaming.util;
+
+public class HeartbeatsTimer {
+    Logger LOG = new Logger(getClass().getSimpleName(), getClass());
+    private long intervalTimeMs;
+    private long lastReportTime = 0;
+
+    public HeartbeatsTimer(){
+        this(120);
+    }
+    public HeartbeatsTimer(long intervalTimeSecond){
+        this.intervalTimeMs = intervalTimeSecond * 1000;
+    }
+
+
+    public boolean isTimeToReport(){
+        boolean b = System.currentTimeMillis() - lastReportTime >= intervalTimeMs;
+        if(b){
+            lastReportTime = System.currentTimeMillis();
+        }
+        return b;
+    }
+
+}
